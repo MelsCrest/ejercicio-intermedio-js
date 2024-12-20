1e129a3604f845324f12adfcc653019561e62e61
MelsCrest <melisacresta@gmail.com>
2024-12-20 09:19:38 +0100

    event addEventListener created

--- js/main.js
+++ js/main.js
@@ -18,3 +18,11 @@ const resultMsg = document.
 querySelector('.js-result-msg');
 const playerPoints = document.querySelector('.js-player-points');
 const computerPoints = document.querySelector('.js-computer-points');
+
+function handleClick(event){
+    event.preventDefault();
+    const valueSelect =  optionSelect.value; //recoge opción elegida por la usuaria
+    console.log(valueSelect);
+};
+
+btn.addEventListener('click', handleClick);
