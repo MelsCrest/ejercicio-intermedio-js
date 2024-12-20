db50dedc573897ba111bbdaac016302a0a356d2d
MelsCrest <melisacresta@gmail.com>
2024-12-20 11:25:38 +0100

    style  added

--- js/main.js
+++ js/main.js
@@ -20,14 +20,16 @@ querySelector('.js-result-msg');
 const playerPoints = document.querySelector('.js-player-points');
 const computerPoints = document.querySelector('.js-computer-points');
 
+//los hago globales para que no se reseteen cada vez que se ejecuta la función y así se guarden los valores nuevos.
 let numPlayer = 0;
 let numPC = 0;
 
-//generar número aleatorio
+// Número aleatorio
 const getRandomNumber = (max) => {
     return Math.ceil(Math.random()*max);
 };
 
+// Partida ordenador
 function computerPlay(){
     const randomNum = getRandomNumber(9);
     let computerPlay = '';
@@ -43,6 +45,7 @@ function computerPlay(){
     return computerPlay;
 };
 
+// Partida usuaria - ordenador
 function compareResult(computerPlay, userPlay){
     
     if(computerPlay === userPlay){
@@ -71,14 +74,16 @@ function compareResult(computerPlay, userPlay){
     computerPoints.innerHTML = numPC;
 };
 
-//recoge opción elegida por la usuaria
+// Resultado de la jugada
 function handleClick(event){
     event.preventDefault();
+    //recoge opción elegida por la usuaria
     const valueSelect =  optionSelect.value; 
-    
+    //recoge opción elegida por el ordenador
     let pcPlay = computerPlay();
-    console.log(pcPlay);
+    //console.log(pcPlay);
     compareResult(pcPlay, valueSelect);
 };
 
+// Generar jugada
 btn.addEventListener('click', handleClick);
