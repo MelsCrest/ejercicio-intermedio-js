4f11ef467bf8b0b055e2dddb55d880f7fb4d21df
MelsCrest <melisacresta@gmail.com>
2024-12-20 09:12:20 +0100

    HTML elements and JS variables created

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 'use strict';
+/*
+    1.- Crear HTML (títulos, select, párrafos).
+    2.-Crear las variables donde se guardan los elementos HTML a interactuar.
+    3.- Escuchar eventos sobre el botón.
+        - generar número aleatorio entre 1 y 9.
+        - hacer comparaciones:
+            - si es menor o igual a 3 -> piedra
+            - si es mayor o igual a 7 -> papel
+            - sino -> tijera
+        - pintar en el HTML los resultados correspondientes:
+            - Empate / Has ganado / Has perdido
+            - puntos
+*/
+const optionSelect = document.querySelector('.js-select-option');
+const btn = document.querySelector('.js-btn');
+const resultMsg = document.
+querySelector('.js-result-msg');
+const playerPoints = document.querySelector('.js-player-points');
+const computerPoints = document.querySelector('.js-computer-points');
